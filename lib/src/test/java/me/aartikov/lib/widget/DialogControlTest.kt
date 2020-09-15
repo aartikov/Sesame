33f7caf7b2dfb1a16e7755050bdd661491b6c71f
Alexander Rovnov <asrovnov@mail.ru>
2020-09-15 11:23:10 +0300

    Remove unnecessary tests and change name tests

--- lib/src/test/java/me/aartikov/lib/widget/DialogControlTest.kt
+++ lib/src/test/java/me/aartikov/lib/widget/DialogControlTest.kt
@@ -1,93 +1,38 @@
 package me.aartikov.lib.widget
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
-import me.aartikov.lib.widget.dialog_control.DialogControl
-import me.aartikov.lib.widget.dialog_control.dialogControl
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 
-class DialogControlTest : ViewModel(), WidgetHost {
-
-    companion object {
-        private const val TEST_SHOW_DIALOG = "Test show dialog"
-    }
-
-    private lateinit var dialogControl: DialogControl<String, String>
-
-    override val widgetHostScope get() = viewModelScope
-
-    @Before
-    fun `set up`() {
-        dialogControl = dialogControl()
-    }
+class DialogControlTest {
 
     @Test
-    fun `show dialog`() = runBlockingTest {
+    fun `accepts result`() = runBlockingTest {
+        val dialogControl = dialogControl<String, String>()
+
+        var actual: String? = null
         val job = launch {
-            dialogControl.show(TEST_SHOW_DIALOG)
+            actual = dialogControl.showForResult("Anything")
         }
-
-        assertEquals(dialogControl.data.value, TEST_SHOW_DIALOG)
-
-        job.cancel()
-    }
-
-    @Test
-    fun `show for result dialog`() = runBlockingTest {
-        val job = launch {
-            dialogControl.showForResult(TEST_SHOW_DIALOG)
-        }
-
-        assertEquals(dialogControl.data.value, TEST_SHOW_DIALOG)
-
-        job.cancel()
-    }
-
-    @Test
-    fun `removed on dismiss`() = runBlockingTest {
-        val job = launch {
-            dialogControl.showForResult(TEST_SHOW_DIALOG)
-        }
-
-        dialogControl.dismiss()
-
-        assertEquals(dialogControl.data.value, null)
-
-        job.cancel()
-    }
-
-    @Test
-    fun `accept result`() = runBlockingTest {
-        var expected: String? = null
-
-        val job = launch {
-            expected = dialogControl.showForResult(TEST_SHOW_DIALOG)
-        }
-
         dialogControl.sendResult("Value 1")
 
-        assertEquals(expected, "Value 1")
-
+        assertEquals( "Value 1", actual)
         job.cancel()
     }
 
     @Test
-    fun `accept one result`() = runBlockingTest {
-        var expected: String? = null
+    fun `skips result when not requested`() = runBlockingTest {
+        val dialogControl = dialogControl<String, String>()
 
-        val job = launch {
-            expected = dialogControl.showForResult(TEST_SHOW_DIALOG)
-        }
-
+        var actual: String? = null
         dialogControl.sendResult("Value 1")
+        val job = launch {
+            actual = dialogControl.showForResult("Anything")
+        }
         dialogControl.sendResult("Value 2")
 
-        assertEquals(expected, "Value 1")
-
+        assertEquals(actual, "Value 2")
         job.cancel()
     }
 }
