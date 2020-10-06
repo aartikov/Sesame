fd70c0981630a61a4ee5945943dbd344c0cd0801
a.artikov <a.artikov@gmail.com>
2020-10-06 17:33:20 +0300

    Refactor computed implementation

--- lib/src/main/java/me/aartikov/lib/data_binding/Computed.kt
+++ lib/src/main/java/me/aartikov/lib/data_binding/Computed.kt
@@ -22,86 +22,99 @@ fun <T, R> PropertyHost.computed(
     return StateDelegate(resultFlow)
 }
 
+@Suppress("UNCHECKED_CAST")
 fun <T1, T2, R> PropertyHost.computed(
     property1: KProperty0<T1>,
     property2: KProperty0<T2>,
     transform: (T1, T2) -> R
 ): StateDelegate<R> {
-    val flow1 = property1.flow
-    val flow2 = property2.flow
-
-    val pairFlow = MutableStateFlow(Pair(flow1.value, flow2.value))
-    val resultFlow = MutableStateFlow(transform(flow1.value, flow2.value))
-
-    propertyHostScope.launch {
-        flow1
-            .drop(1)
-            .collect {
-                pairFlow.value = pairFlow.value.copy(first = it)
-            }
+    return computedImpl(property1, property2) { args: List<*> ->
+        transform(
+            args[0] as T1,
+            args[1] as T2
+        )
     }
-
-    propertyHostScope.launch {
-        flow2
-            .drop(1)
-            .collect {
-                pairFlow.value = pairFlow.value.copy(second = it)
-            }
-    }
-
-    propertyHostScope.launch {
-        pairFlow
-            .drop(1)
-            .collect {
-                resultFlow.value = transform(it.first, it.second)
-            }
-    }
-
-    return StateDelegate(resultFlow)
 }
 
+@Suppress("UNCHECKED_CAST")
 fun <T1, T2, T3, R> PropertyHost.computed(
     property1: KProperty0<T1>,
     property2: KProperty0<T2>,
     property3: KProperty0<T3>,
     transform: (T1, T2, T3) -> R
 ): StateDelegate<R> {
-    val flow1 = property1.flow
-    val flow2 = property2.flow
-    val flow3 = property3.flow
+    return computedImpl(property1, property2, property3) { args: List<*> ->
+        transform(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3
+        )
+    }
+}
 
-    val tripleFlow = MutableStateFlow(Triple(flow1.value, flow2.value, flow3.value))
-    val resultFlow = MutableStateFlow(transform(flow1.value, flow2.value, flow3.value))
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, R> PropertyHost.computed(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    transform: (T1, T2, T3, T4) -> R
+): StateDelegate<R> {
+    return computedImpl(property1, property2, property3, property4) { args: List<*> ->
+        transform(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4
+        )
+    }
+}
 
-    propertyHostScope.launch {
-        flow1
-            .drop(1)
-            .collect {
-                tripleFlow.value = tripleFlow.value.copy(first = it)
-            }
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, R> PropertyHost.computed(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    property5: KProperty0<T5>,
+    transform: (T1, T2, T3, T4, T5) -> R
+): StateDelegate<R> {
+    return computedImpl(property1, property2, property3, property4, property5) { args: List<*> ->
+        transform(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5
+        )
+    }
+}
+
+private inline fun <T, R> PropertyHost.computedImpl(
+    vararg properties: KProperty0<T>,
+    crossinline transform: (List<T>) -> R
+): StateDelegate<R> {
+
+    val flows = properties.map { it.flow }
+    val initialValues = flows.map { it.value }
+    val elementsFlow = MutableStateFlow(initialValues)
+    val resultFlow = MutableStateFlow(transform(initialValues))
+
+    flows.forEachIndexed { index, flow ->
+        propertyHostScope.launch {
+            flow
+                .drop(1)
+                .collect {
+                    elementsFlow.value = elementsFlow.value.toMutableList().apply { this[index] = it }
+                }
+        }
     }
 
     propertyHostScope.launch {
-        flow2
+        elementsFlow
             .drop(1)
             .collect {
-                tripleFlow.value = tripleFlow.value.copy(second = it)
-            }
-    }
-
-    propertyHostScope.launch {
-        flow3
-            .drop(1)
-            .collect {
-                tripleFlow.value = tripleFlow.value.copy(third = it)
-            }
-    }
-
-    propertyHostScope.launch {
-        tripleFlow
-            .drop(1)
-            .collect {
-                resultFlow.value = transform(it.first, it.second, it.third)
+                resultFlow.value = transform(it)
             }
     }
 
