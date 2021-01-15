2d72493c3f9c4abfe24cd6e29921a2520a872a59
Artur Artikov <artur.artikov@mobileup.ru>
2021-01-15 14:43:11 +0300

    Use flow for command implementation

--- lib/src/main/java/me/aartikov/lib/data_binding/PropertyObserver.kt
+++ lib/src/main/java/me/aartikov/lib/data_binding/PropertyObserver.kt
@@ -2,6 +2,7 @@ package me.aartikov.lib.data_binding
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlin.reflect.KProperty0
@@ -10,7 +11,7 @@ interface PropertyObserver {
 
     val propertyObserverLifecycleOwner: LifecycleOwner
 
-    infix fun <T> StateFlow<T>.bind(consumer: (T) -> Unit) {
+    infix fun <T> Flow<T>.bind(consumer: (T) -> Unit) {
         propertyObserverLifecycleOwner.lifecycleScope.launchWhenStarted {
             this@bind.collect {
                 consumer(it)
@@ -23,10 +24,6 @@ interface PropertyObserver {
     }
 
     infix fun <T> Command<T>.bind(consumer: (T) -> Unit) {
-        propertyObserverLifecycleOwner.lifecycleScope.launchWhenStarted {
-            while (true) {
-                consumer(this@bind.receive())
-            }
-        }
+        this.flow bind consumer
     }
 }
