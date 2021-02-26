ca819ccb50a503e8aeba48ee30d17da6716ff018
Artur Artikov <artur.artikov@mobileup.ru>
2021-02-26 19:08:11 +0300

    Fix navigation in sample

--- sample/src/main/kotlin/me/aartikov/sesamesample/MainActivity.kt
+++ sample/src/main/kotlin/me/aartikov/sesamesample/MainActivity.kt
@@ -3,15 +3,16 @@ package me.aartikov.sesamesample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import me.aartikov.sesame.navigation.NavigationMessage
+import me.aartikov.sesame.navigation.NavigationMessageDispatcher
+import me.aartikov.sesame.navigation.NavigationMessageHandler
+import me.aartikov.sesamesample.base.BaseFragment
 import me.aartikov.sesamesample.counter.CounterFragment
 import me.aartikov.sesamesample.dialogs.ClockFragment
 import me.aartikov.sesamesample.dialogs.DialogsFragment
 import me.aartikov.sesamesample.menu.MenuFragment
 import me.aartikov.sesamesample.movies.ui.MoviesFragment
 import me.aartikov.sesamesample.profile.ui.ProfileFragment
-import me.aartikov.sesame.navigation.NavigationMessage
-import me.aartikov.sesame.navigation.NavigationMessageDispatcher
-import me.aartikov.sesame.navigation.NavigationMessageHandler
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -36,7 +37,10 @@ class MainActivity : AppCompatActivity(), NavigationMessageHandler {
 
     override fun handleNavigationMessage(message: NavigationMessage): Boolean {
         when (message) {
-            is Back -> navigator.back()
+            is Back -> {
+                val success = navigator.back()
+                if (!success) finish()
+            }
             is OpenCounterScreen -> navigator.goTo(CounterFragment())
             is OpenProfileScreen -> navigator.goTo(ProfileFragment())
             is OpenDialogsScreen -> navigator.goTo(DialogsFragment())
@@ -47,6 +51,6 @@ class MainActivity : AppCompatActivity(), NavigationMessageHandler {
     }
 
     override fun onBackPressed() {
-        if (!navigator.back()) finish()
+        (navigator.currentScreen as? BaseFragment<*>)?.onBackPressed()
     }
 }
