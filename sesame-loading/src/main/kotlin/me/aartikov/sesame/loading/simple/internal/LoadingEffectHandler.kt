978e63028e71ade54bee497d2e3c027a1ea320e1
Artur Artikov <artur.artikov@mobileup.ru>
2021-03-30 00:05:53 +0300

    Rename  OrdinaryLoading argument loader to load

--- sesame-loading/src/main/kotlin/me/aartikov/sesame/loading/simple/internal/LoadingEffectHandler.kt
+++ sesame-loading/src/main/kotlin/me/aartikov/sesame/loading/simple/internal/LoadingEffectHandler.kt
@@ -7,19 +7,19 @@ import me.aartikov.sesame.loading.simple.isEmpty
 import me.aartikov.sesame.loop.EffectHandler
 import java.util.concurrent.CancellationException
 
-internal class LoadingEffectHandler<T : Any>(private val loader: suspend (fresh: Boolean) -> T?) :
+internal class LoadingEffectHandler<T : Any>(private val load: suspend (fresh: Boolean) -> T?) :
     EffectHandler<Effect<T>, Action<T>> {
 
     private var job: Job? = null
 
     override suspend fun handleEffect(effect: Effect<T>, actionConsumer: (Action<T>) -> Unit) {
         when (effect) {
-            is Effect.Load -> load(effect.fresh, actionConsumer)
+            is Effect.Load -> loadData(effect.fresh, actionConsumer)
             is Effect.CancelLoading -> cancelLoading()
         }
     }
 
-    private suspend fun load(
+    private suspend fun loadData(
         fresh: Boolean,
         actionConsumer: (Action<T>) -> Unit
     ) = coroutineScope {
