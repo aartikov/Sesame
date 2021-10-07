25dac056799bc424f9d60184bc782237e37520cb
Artur Artikov <artur.artikov@mobileup.ru>
2021-10-07 11:36:00 +0300

    Use Dispatchers.Main.immediate for component coroutine scope

--- compose-sample/src/main/kotlin/me/aartikov/sesamecomposesample/utils/ComponentCoroutineScope.kt
+++ compose-sample/src/main/kotlin/me/aartikov/sesamecomposesample/utils/ComponentCoroutineScope.kt
@@ -3,11 +3,12 @@ package me.aartikov.sesamecomposesample.utils
 import com.arkivanov.essenty.lifecycle.LifecycleOwner
 import com.arkivanov.essenty.lifecycle.doOnDestroy
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 
 fun LifecycleOwner.componentCoroutineScope(): CoroutineScope {
-    val scope = MainScope()
+    val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
     lifecycle.doOnDestroy {
         scope.cancel()
     }
