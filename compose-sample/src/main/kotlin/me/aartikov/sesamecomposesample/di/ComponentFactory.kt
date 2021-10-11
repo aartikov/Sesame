d5989211df21d254afb26d93550ad310bd758b38
Angelina Podbolotova <a.podbolotova@mintmail.ru>
2021-10-11 09:44:48 +0300

    add message service and change start koin

--- compose-sample/src/main/kotlin/me/aartikov/sesamecomposesample/di/ComponentFactory.kt
+++ compose-sample/src/main/kotlin/me/aartikov/sesamecomposesample/di/ComponentFactory.kt
@@ -7,22 +7,30 @@ import me.aartikov.sesamecomposesample.menu.MenuComponent
 import me.aartikov.sesamecomposesample.menu.RealMenuComponent
 import me.aartikov.sesamecomposesample.movies.ui.RealMoviesComponent
 import me.aartikov.sesamecomposesample.profile.ui.RealProfileComponent
+import me.aartikov.sesamecomposesample.root.RealRootComponent
+import org.koin.core.Koin
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
+import org.koin.dsl.module
 
 class ComponentFactory : KoinComponent {
 
+    override fun getKoin(): Koin = ComponentFactoryModule.koin ?: throw NullPointerException()
+
     fun createProfileComponent(componentContext: ComponentContext) = RealProfileComponent(
         componentContext,
+        get(),
         get()
     )
 
     fun createDialogsComponent(componentContext: ComponentContext) = RealDialogsComponent(
-        componentContext
+        componentContext,
+        get()
     )
 
     fun createCounterComponent(componentContext: ComponentContext) = RealCounterComponent(
-        componentContext
+        componentContext,
+        get()
     )
 
     fun createMenuComponent(
@@ -35,6 +43,22 @@ class ComponentFactory : KoinComponent {
 
     fun createMoviesComponent(componentContext: ComponentContext) = RealMoviesComponent(
         componentContext,
+        get(),
         get()
     )
+
+    fun createRootComponent(componentContext: ComponentContext) = RealRootComponent(
+        componentContext,
+        get()
+    )
+}
+
+object ComponentFactoryModule {
+
+    var koin : Koin? = null
+
+    fun create() = module {
+
+         factory { ComponentFactory() }
+    }
 }
