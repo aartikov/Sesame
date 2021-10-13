9a5ed37ac2f148bd41eb5697a6a96bd41b595616
Artur Artikov <artur.artikov@mobileup.ru>
2021-10-13 15:30:25 +0300

    Add more arity for computed and autorun

--- sesame-property/src/main/kotlin/me/aartikov/sesame/property/Autorun.kt
+++ sesame-property/src/main/kotlin/me/aartikov/sesame/property/Autorun.kt
@@ -141,6 +141,164 @@ fun <T1, T2, T3, T4, T5> PropertyHost.autorun(
     }
 }
 
+/**
+ * Runs a [block] whenever any of the given observable properties is changed.
+ * @param debounceTimeoutMillis optional debounce setting.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6> PropertyHost.autorun(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    property5: KProperty0<T5>,
+    property6: KProperty0<T6>,
+    debounceTimeoutMillis: Long? = null,
+    block: (T1, T2, T3, T4, T5, T6) -> Unit
+) {
+    autorunImpl(
+        property1,
+        property2,
+        property3,
+        property4,
+        property5,
+        property6,
+        debounceTimeoutMillis = debounceTimeoutMillis
+    ) { args: List<*> ->
+        block(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5,
+            args[5] as T6
+        )
+    }
+}
+
+/**
+ * Runs a [block] whenever any of the given observable properties is changed.
+ * @param debounceTimeoutMillis optional debounce setting.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, T7> PropertyHost.autorun(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    property5: KProperty0<T5>,
+    property6: KProperty0<T6>,
+    property7: KProperty0<T7>,
+    debounceTimeoutMillis: Long? = null,
+    block: (T1, T2, T3, T4, T5, T6, T7) -> Unit
+) {
+    autorunImpl(
+        property1,
+        property2,
+        property3,
+        property4,
+        property5,
+        property6,
+        property7,
+        debounceTimeoutMillis = debounceTimeoutMillis
+    ) { args: List<*> ->
+        block(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5,
+            args[5] as T6,
+            args[6] as T7
+        )
+    }
+}
+
+/**
+ * Runs a [block] whenever any of the given observable properties is changed.
+ * @param debounceTimeoutMillis optional debounce setting.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, T7, T8> PropertyHost.autorun(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    property5: KProperty0<T5>,
+    property6: KProperty0<T6>,
+    property7: KProperty0<T7>,
+    property8: KProperty0<T8>,
+    debounceTimeoutMillis: Long? = null,
+    block: (T1, T2, T3, T4, T5, T6, T7, T8) -> Unit
+) {
+    autorunImpl(
+        property1,
+        property2,
+        property3,
+        property4,
+        property5,
+        property6,
+        property7,
+        property8,
+        debounceTimeoutMillis = debounceTimeoutMillis
+    ) { args: List<*> ->
+        block(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5,
+            args[5] as T6,
+            args[6] as T7,
+            args[7] as T8
+        )
+    }
+}
+
+/**
+ * Runs a [block] whenever any of the given observable properties is changed.
+ * @param debounceTimeoutMillis optional debounce setting.
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T1, T2, T3, T4, T5, T6, T7, T8, T9> PropertyHost.autorun(
+    property1: KProperty0<T1>,
+    property2: KProperty0<T2>,
+    property3: KProperty0<T3>,
+    property4: KProperty0<T4>,
+    property5: KProperty0<T5>,
+    property6: KProperty0<T6>,
+    property7: KProperty0<T7>,
+    property8: KProperty0<T8>,
+    property9: KProperty0<T9>,
+    debounceTimeoutMillis: Long? = null,
+    block: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> Unit
+) {
+    autorunImpl(
+        property1,
+        property2,
+        property3,
+        property4,
+        property5,
+        property6,
+        property7,
+        property8,
+        property9,
+        debounceTimeoutMillis = debounceTimeoutMillis
+    ) { args: List<*> ->
+        block(
+            args[0] as T1,
+            args[1] as T2,
+            args[2] as T3,
+            args[3] as T4,
+            args[4] as T5,
+            args[5] as T6,
+            args[6] as T7,
+            args[7] as T8,
+            args[8] as T9
+        )
+    }
+}
+
 private inline fun <T> PropertyHost.autorunImpl(
     vararg properties: KProperty0<T>,
     debounceTimeoutMillis: Long?,
